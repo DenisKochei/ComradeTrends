a562a26e05d43ad607c4a4c103bd116d5a4bc387
DenisKochei <denisdante.254@gmail.com>
2025-03-05 10:53:26 +0300

    jwt authentication fix trial

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,24 +1,24 @@
-import User from "../models/user.model.js";
-import bcryptjs from "bcryptjs";
-import { errorHandler } from "../utils/error.js";
-import jwt from "jsonwebtoken";
+
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
+
   if (
     !username ||
     !email ||
     !password ||
-    username === "" ||
-    email === "" ||
-    password === ""
+    username === '' ||
+    email === '' ||
+    password === ''
   ) {
-    next(errorHandler(400, "All fields are required"));
+    next(errorHandler(400, 'All fields are required'));
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
-
-  //The "10" is the the salt rounds. It represents the number of times bcrypt stirs the cauldron (i.e., the number of hashing rounds).
 
   const newUser = new User({
     username,
@@ -28,34 +28,28 @@ export const signup = async (req, res, next) => {
 
   try {
     await newUser.save();
-    res.json({ message: "Signup Successful" });
-  } catch (err) {
-    if (err.message.includes("email_1 dup key")) {
-      next(errorHandler(400, "The email is already in use"));
-    }
-    next(errorHandler(400, "Network interuption,please try again"));
+    res.json('Signup successful');
+  } catch (error) {
+    next(error);
   }
 };
 
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
 
-  if (!email || !password || email === "" || password === "") {
-    next(errorHandler(400, "All fields are required"));
+  if (!email || !password || email === '' || password === '') {
+    next(errorHandler(400, 'All fields are required'));
   }
+
   try {
     const validUser = await User.findOne({ email });
-
     if (!validUser) {
-      return next(errorHandler(404, "User not found, please signup"));
+      return next(errorHandler(404, 'User not found'));
     }
     const validPassword = bcryptjs.compareSync(password, validUser.password);
-
-    //password is what we get from the form while validuser.passowrd is what database
     if (!validPassword) {
-      return next(errorHandler(400, "Invalid Password or email"));
+      return next(errorHandler(400, 'Invalid password'));
     }
-    //When comparing both the email and the password it is best to make the message not clear by saying that either or both the email or the password is incorrect
     const token = jwt.sign(
       { id: validUser._id, isAdmin: validUser.isAdmin },
       process.env.JWT_SECRET
@@ -65,18 +59,15 @@ export const signin = async (req, res, next) => {
 
     res
       .status(200)
-      .cookie("access_token", token, {
+      .cookie('access_token', token, {
         httpOnly: true,
       })
       .json(rest);
-  } catch (err) {
-    if (err.message.includes("buffering")) {
-      next(errorHandler(400, "network interuption,please try again"));
-    } else {
-      next(errorHandler(400, "Network interuption,please try again"));
-    }
+  } catch (error) {
+    next(error);
   }
 };
+
 export const google = async (req, res, next) => {
   const { email, name, googlePhotoUrl } = req.body;
   try {
@@ -89,7 +80,7 @@ export const google = async (req, res, next) => {
       const { password, ...rest } = user._doc;
       res
         .status(200)
-        .cookie("access_token", token, {
+        .cookie('access_token', token, {
           httpOnly: true,
         })
         .json(rest);
@@ -100,7 +91,7 @@ export const google = async (req, res, next) => {
       const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
       const newUser = new User({
         username:
-          name.toLowerCase().split(" ").join("") +
+          name.toLowerCase().split(' ').join('') +
           Math.random().toString(9).slice(-4),
         email,
         password: hashedPassword,
@@ -114,7 +105,7 @@ export const google = async (req, res, next) => {
       const { password, ...rest } = newUser._doc;
       res
         .status(200)
-        .cookie("access_token", token, {
+        .cookie('access_token', token, {
           httpOnly: true,
         })
         .json(rest);
