9ccbe73a680313275ab5d8e1e1a52f46688aec13
DenisKochei <denisdante.254@gmail.com>
2025-03-05 12:19:30 +0300

    fix 3

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,24 +1,24 @@
-
-import User from '../models/user.model.js';
-import bcryptjs from 'bcryptjs';
-import { errorHandler } from '../utils/error.js';
-import jwt from 'jsonwebtoken';
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-
   if (
     !username ||
     !email ||
     !password ||
-    username === '' ||
-    email === '' ||
-    password === ''
+    username === "" ||
+    email === "" ||
+    password === ""
   ) {
-    next(errorHandler(400, 'All fields are required'));
+    next(errorHandler(400, "All fields are required"));
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
+
+  //The "10" is the the salt rounds. It represents the number of times bcrypt stirs the cauldron (i.e., the number of hashing rounds).
 
   const newUser = new User({
     username,
@@ -28,30 +28,36 @@ export const signup = async (req, res, next) => {
 
   try {
     await newUser.save();
-    res.json('Signup successful');
-  } catch (error) {
-    next(error);
+    res.json({ message: "Signup Successful" });
+  } catch (err) {
+    if (err.message.includes("email_1 dup key")) {
+      next(errorHandler(400, "The email is already in use"));
+    }
+    next(errorHandler(400, "Network interuption,please try again"));
   }
 };
 
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
 
-  if (!email || !password || email === '' || password === '') {
-    next(errorHandler(400, 'All fields are required'));
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required"));
   }
-
   try {
     const validUser = await User.findOne({ email });
+
     if (!validUser) {
-      return next(errorHandler(404, 'User not found'));
+      return next(errorHandler(404, "User not found, please signup"));
     }
     const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+    //password is what we get from the form while validuser.passowrd is what database
     if (!validPassword) {
-      return next(errorHandler(400, 'Invalid password'));
+      return next(errorHandler(400, "Invalid Password or email"));
     }
+    //When comparing both the email and the password it is best to make the message not clear by saying that either or both the email or the password is incorrect
     const token = jwt.sign(
-      { id: validUser._id},
+      { id: validUser._id, isAdmin: validUser.isAdmin },
       process.env.JWT_SECRET
     );
 
@@ -59,28 +65,31 @@ export const signin = async (req, res, next) => {
 
     res
       .status(200)
-      .cookie('access_token', token, {
+      .cookie("access_token", token, {
         httpOnly: true,
       })
       .json(rest);
-  } catch (error) {
-    next(error);
+  } catch (err) {
+    if (err.message.includes("buffering")) {
+      next(errorHandler(400, "network interuption,please try again"));
+    } else {
+      next(errorHandler(400, "Network interuption,please try again"));
+    }
   }
 };
-
 export const google = async (req, res, next) => {
   const { email, name, googlePhotoUrl } = req.body;
   try {
     const user = await User.findOne({ email });
     if (user) {
       const token = jwt.sign(
-        { id: user._id},
+        { id: user._id, isAdmin: user.isAdmin },
         process.env.JWT_SECRET
       );
       const { password, ...rest } = user._doc;
       res
         .status(200)
-        .cookie('access_token', token, {
+        .cookie("access_token", token, {
           httpOnly: true,
         })
         .json(rest);
@@ -91,7 +100,7 @@ export const google = async (req, res, next) => {
       const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
       const newUser = new User({
         username:
-          name.toLowerCase().split(' ').join('') +
+          name.toLowerCase().split(" ").join("") +
           Math.random().toString(9).slice(-4),
         email,
         password: hashedPassword,
@@ -99,13 +108,13 @@ export const google = async (req, res, next) => {
       });
       await newUser.save();
       const token = jwt.sign(
-        { id: newUser._id},
+        { id: newUser._id, isAdmin: newUser.isAdmin },
         process.env.JWT_SECRET
       );
       const { password, ...rest } = newUser._doc;
       res
         .status(200)
-        .cookie('access_token', token, {
+        .cookie("access_token", token, {
           httpOnly: true,
         })
         .json(rest);
@@ -114,12 +123,3 @@ export const google = async (req, res, next) => {
     next(error);
   }
 };
-
-export const signOut = async (req, res, next) => {
-  try {
-    res.clearCookie('access_token');
-    res.status(200).json('User has been logged out!');
-  } catch (error) {
-    next(error);
-  }
-}
