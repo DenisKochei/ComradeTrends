8a83e5eb0ed8ba24a704e3fdd57a92515c5e125f
DenisKochei <denisdante.254@gmail.com>
2025-03-05 11:34:25 +0300

    returned verifyUser

--- backend/routes/comment.route.js
+++ backend/routes/comment.route.js
@@ -4,11 +4,11 @@ import { createComment,getPostComments,likeComment,editComment,deleteComment,get
 
 const router = express.Router();
 
-router.post('/create', createComment)
+router.post('/create',verifyToken, createComment)
 router.get('/getPostComments', getPostComments);
-router.put('/likeComment/:commentId',likeComment)
-router.put('/editComment/:commentId',editComment)
-router.delete('/deleteComment/:commentId',deleteComment)
-router.get('/getAllComments', getAllComments);
+router.put('/likeComment/:commentId',verifyToken,likeComment)
+router.put('/editComment/:commentId',verifyToken,editComment)
+router.delete('/deleteComment/:commentId',verifyToken,deleteComment)
+router.get('/getAllComments', verifyToken, getAllComments);
 
 export default router;
--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -4,9 +4,9 @@ import { create,getposts ,deletepost,updatepost} from '../controllers/post.contr
 
 const router = express.Router();
 
-router.post('/create',create)
+router.post('/create',verifyToken,create)
 router.get('/getposts',getposts)
-router.delete('/deletepost/:postId/:userId',deletepost)
-router.put('/updatepost/:postId/:userId',updatepost)
+router.delete('/deletepost/:postId/:userId',verifyToken,deletepost)
+router.put('/updatepost/:postId/:userId',verifyToken,updatepost)
 
 export default router;
