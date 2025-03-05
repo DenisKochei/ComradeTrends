bad3a63009d6e21aa3513b65fa066537d298c468
DenisKochei <denisdante.254@gmail.com>
2025-03-05 10:28:06 +0300

    persistor fix trial

--- frontend/redux/store.js
+++ frontend/redux/store.js
@@ -1,28 +1,27 @@
-import { configureStore,combineReducers } from '@reduxjs/toolkit';
-import userReducer from './user/userSlice'
-import {persistReducer} from 'redux-persist'
-import storage from 'redux-persist/lib/storage';
-import persistStore from 'redux-persist/es/persistStore';
-import ThemeReducer from './theme/themeSlice'
 
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import themeReducer from './theme/themeSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const rootReducer = combineReducers({
-  user : userReducer,
-  theme : ThemeReducer,
-})
+  user: userReducer,
+  theme: themeReducer,
+});
 
 const persistConfig = {
   key: 'root',
   storage,
-  version : 1,
-}
+  version: 1,
+};
 
-const persistedReducer = persistReducer(persistConfig,rootReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer : persistedReducer,
-  middleware : (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck:false})
-  //this prevents errors
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+});
 
-export const persistor = persistStore(store)
+export const persistor = persistStore(store);
