9a6faf37c0edeaa73559c4a54a0ea3c32d99ccc0
DenisKochei <denisdante.254@gmail.com>
2025-04-11 14:01:31 +0300

    post card

--- frontend/src/components/PostCard.jsx
+++ frontend/src/components/PostCard.jsx
@@ -15,8 +15,11 @@ export function PostCard({ post }) {
         />
       </div>
       <div className="p-3 flex flex-col gap-2">
-        <p className="text-lg font-semibold line-clamp-2">{post.title}</p>
-        <span className="italic text-xs">{post.category}</span>
+        <p className="text-md font-semibold line-clamp-2">{post.title}</p>
+       <div className="flex justify-start items-center">
+       <span className="italic text-slate-600 text-xs">{post.category}</span>
+       <span className="text-slate-600">{post.subcategory ? `/ ${post.subcategory}` : ""}</span>
+       </div>
         <span className="z-10 group-hover:bottom-0 absolute bottom-[-200px] left-0 right-0 border border-teal-500 text-teal-500 hover:bg-teal-500 hover:text-white transition-all duration-300 text-center py-2 rounded-md !rounded-tl-none m-2">
           Read Article
         </span>
