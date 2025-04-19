fa349cde51231531d36d09dba71a0acb0f874c1d
DenisKochei <denisdante.254@gmail.com>
2025-04-19 20:14:56 +0300

    frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,30 +13,5 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
-      manifest: {
-        name: 'Comrade Trends App',
-        short_name: 'ComradeTrends',
-        description: "Kenya's and the Globe's #1 news and entertainment website",
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        start_url: '/',
-        icons: [
-          {
-            src: 'icons/ComradeTrendsLogo1.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'icons/ComradeTrendsLogo2.png',
-            sizes: '512x512',
-            type: 'image/png',
-          }
-        ]
-      }
-    })
   ]
 });
