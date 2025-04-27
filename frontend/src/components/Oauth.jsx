ee175682e7af22f0db95143be08d12256cd29303
DenisKochei <denisdante.254@gmail.com>
2025-04-27 13:41:50 +0300

    fix 1-6

--- frontend/src/components/Oauth.jsx
+++ frontend/src/components/Oauth.jsx
@@ -4,12 +4,14 @@ import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
 import { app } from "../firebase";
 import { useDispatch } from "react-redux";
 import { signinSuccess } from "../../redux/user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 export default function Oauth() {
   const auth = getAuth(app);
+  const navigate = useNavigate()
   const dispatch = useDispatch();
   const goBack = () => {
-    window.history.back();
+    ()=>navigate("/")
   };
 
   const handleGoogleClick = async () => {
