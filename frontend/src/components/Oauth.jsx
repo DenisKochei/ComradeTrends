452cbfa2b9b8bbc5c343abe7f6758279e5da3c04
DenisKochei <denisdante.254@gmail.com>
2025-04-27 13:10:09 +0300

    fix1.1

--- frontend/src/components/Oauth.jsx
+++ frontend/src/components/Oauth.jsx
@@ -4,7 +4,6 @@ import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
 import { app } from "../firebase";
 import { useDispatch } from "react-redux";
 import { signinSuccess } from "../../redux/user/userSlice";
-import { useNavigate } from "react-router-dom";
 
 export default function Oauth() {
   const auth = getAuth(app);
