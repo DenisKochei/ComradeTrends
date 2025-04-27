6595640082e2577ea4bed6178a6b786c963d0ee4
DenisKochei <denisdante.254@gmail.com>
2025-04-27 18:33:33 +0300

    homepost card2 update

--- frontend/src/components/HomePostCard.jsx
+++ frontend/src/components/HomePostCard.jsx
@@ -1,27 +1,30 @@
 import { Link } from "react-router-dom";
-import { Button } from "flowbite-react";
+import moment from "moment";
 
 export function HomePostCard({ post }) {
   return (
     <div>
       <div className="flex flex-col sm:mt-5 mt-2 justify-start items-center">
-       <div className="flex flex-col-reverse text-start sm:flex-col">
-        <h1 className="font-semi font-serif font-bold text-xl">{post.title}</h1>
-       <Link
-          to={`/search?category=${post.category}`}
-          className=" mt-2 flex justify-start items-center gap-1"
-        >
-          <div className={`w-3 h-3 ${post.category === "breaking" ? "bg-red-400" : post.category === "most-trending" ? "bg-blue-400" : "bg-green-400"} rounded-full`}></div>
-          <button className="focus:ring-0 p-0" pill color="gray" size="xs">
-            {post.category === "most-trending"
-              ? "Most Trending"
-              : post.category === 'breaking' ? "Breaking News" : post.category}
-          </button>
-        </Link>
-        <div className=" object-cover my-2 overflow-hidden">
-          <img loading="lazy" className="w-full h-64 sm:h-96 object-cover" src={post.image} />
+        <div className="flex flex-col-reverse text-start sm:flex-col">
+          <h1 className="font-semi font-serif font-bold text-xl">{post.title}</h1>
+          <div className=" flex justify-between items-center mx-1">
+            <Link
+              to={`/search?category=${post.category}`}
+              className=" mt-2 flex justify-start items-center gap-1"
+            >
+              <div className={`w-3 h-3 ${post.category === "breaking" ? "bg-red-400" : post.category === "most-trending" ? "bg-blue-400" : "bg-green-400"} rounded-full`}></div>
+              <button className="focus:ring-0 p-0" pill color="gray" size="xs">
+                {post.category === "most-trending"
+                  ? "Most Trending"
+                  : post.category === 'breaking' ? "Breaking News" : post.category}
+              </button>
+            </Link>
+            <span className=" mt-1 text-nowrap mr-2">{moment(post.createdAt).fromNow()}</span>
+          </div>
+          <div className=" object-cover my-2 overflow-hidden">
+            <img loading="lazy" className="w-full h-64 sm:h-96 object-cover" src={post.image} />
+          </div>
         </div>
-       </div>
         <div
           className="md:line-clamp-3 text-start line-clamp-2"
           dangerouslySetInnerHTML={{ __html: post.content1 }}
