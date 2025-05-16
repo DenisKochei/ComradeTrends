0f754f788e9afdaa5c72549ac8e7006a784c71e2
DenisKochei <denisdante.254@gmail.com>
2025-05-16 18:49:43 +0300

    yheme 1

--- frontend/src/components/ThemeProvider.jsx
+++ frontend/src/components/ThemeProvider.jsx
@@ -4,7 +4,7 @@ export default function ThemeProvider({ children }) {
   const { theme } = useSelector((state) => state.theme);
   return (
     <div className={theme}>
-      <div className="text-gray-700 bg-white dark:text-gray-200 dark:bg-slate-800 min-h-screen">
+      <div className="text-gray-700 dark:text-gray-200 dark:bg-slate-800 min-h-screen">
         {children}
       </div>
     </div>
